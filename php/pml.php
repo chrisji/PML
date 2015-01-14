0c89eb9d694107a35f8bc3dd825fae8814ebda98
CraigChilds94 <childscraig17@gmail.com>
2015-01-14 13:54:25 +0000

    Add namespace to tester

--- php/pml.php
+++ php/pml.php
@@ -5,6 +5,7 @@
 // Put composer autoload here +++++++++++
 
 // Init test class
+use PML\Testing\Tester;
 $tests = new Tester()
     ->addTest('test', function($data) {
         return is_array($data);
