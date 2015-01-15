b6efb17f2600876dccfc9500220516479fac39e0
chrisji <chrisji@hotmail.co.uk>
2015-01-15 18:32:52 +0000

    Commenting Tag class

--- java/PML/src/tags/Tag.java
+++ java/PML/src/tags/Tag.java
@@ -17,12 +17,22 @@ public class Tag {
 	private ArrayList<Tag> childTags;
 	private String tagName;
 	
+	/**
+	 * Constructor for Tag.
+	 * 
+	 * Currently an empty constructor to simplify reflection.
+	 */
 	public Tag() {
 		this.attributes = null;
 		this.childTags = null;
 		this.tagName = null;
 	}
 
+	/**
+	 * Outputs the 'code' for the Tag.
+	 * 
+	 * @param tags ArrayList<Tag> containing this Tag's children.
+	 */
 	public void output(ArrayList<Tag> tags) {
 		System.out.println("<" + this.tagName + buildAttributeString() + ">");
 		PMLParser.parse(tags);
@@ -48,6 +58,11 @@ public class Tag {
 		return this.attributes;
 	}
 	
+	/**
+	 * Returns the Tag's name.
+	 * 
+	 * @return String the Tag's name.
+	 */
 	public String getTagName() {
 		return this.tagName;
 	}
@@ -63,16 +78,30 @@ public class Tag {
 	
 	/**
 	 * Set the attributes for this tag.
+	 * 
 	 * @param HashMap<String, String> - HashMap of attributes.
 	 */
 	public void setAttributes(HashMap<String, String> attributes) {
 		this.attributes = attributes;
 	}
 	
+	/**
+	 * Sets the name of the Tag.
+	 * 
+	 * @param tagName String containing the name of the Tag.
+	 */
 	public void setTagName(String tagName) {
 		this.tagName = tagName;
 	}
 	
+	/**
+	 * Returns a String representing the Tag's attribute map.
+	 * 
+	 * Example: A map containing ("x", "1") and ("y", "hello") 
+	 * returns the string: " x=\"1\" y=\"hello\""
+	 * 
+	 * @return String representing the Tag's attributes.
+	 */
 	public String buildAttributeString() {
 		String attString = "";
 		
@@ -87,7 +116,13 @@ public class Tag {
 	}
 	
 	
+	/**
+	 * Tests whether this Tag has attributes.
+	 * 
+	 * @return boolean true if the Tag has one or more attribute, false otherwise.
+	 */
 	public boolean hasAttributes() {
-		return this.attributes!=null;
+		// Has attributes if the attributes map has been initialised and is not empty.
+		return this.attributes!=null && !this.attributes.isEmpty();
 	}
 }
